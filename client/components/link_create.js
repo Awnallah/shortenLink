4cf0bfbcdf17385303abac39e27fe9ae417017d7
Hamzah <6hamzah6@gmail.com>
2016-08-21 10:01:54 -0700

    form content added

--- client/components/link_create.js
+++ client/components/link_create.js
@@ -1,10 +1,15 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 
 class LinkCreate extends Component {
 	render() {
 		return (
 			<form>
+				<div className='form-group'>
+					<label>Link to shorten </label>
+					<input className='form-control' />
+				</div>
+				<button className="btn btn-primary">Shorten</button>
 			</form>
 			);
 	};
