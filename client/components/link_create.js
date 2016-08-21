6322acf2d91cb673fdb60456dbd8664c504f61c5
Hamzah <6hamzah6@gmail.com>
2016-08-21 09:13:48 -0700

    added collection and form component

---
+++ client/components/link_create.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+class LinkCreate extends Component {
+	render() {
+		return (
+			<form>
+			</form>
+			);
+	};
+}
+
+
+export default LinkCreate;
--- client/main.js
+++ client/main.js
@@ -2,11 +2,16 @@ import React from 'react';
 import ReactDom from 'react-dom';
 
 import Header from './components/header';
+import LinkCreate from './components/link_create';
 
 
 const App=()=>{
 	return(
-		< Header />
+		<div>
+			< Header />
+			<LinkCreate />
+		</div>
+
 		);
 };
 
