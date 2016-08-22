c6efe89afa9f62b35f16ad09f7996233d510ce92
Hamzah <6hamzah6@gmail.com>
2016-08-21 21:03:23 -0700

    click count updates in DB for valid link redirects

--- server/main.js
+++ server/main.js
@@ -15,6 +15,9 @@ function onRoute(req, res, next ){
 	const link = Links.findOne({ token: req.params.token });
 	//redirect user to long URL if link is found
 	if (link){
+
+		Links.update(link, { $inc: { clicks: 1}});
+
 		res.writeHead(307, {'Location':link.url});
 		res.end();
 
